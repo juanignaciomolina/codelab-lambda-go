6d97ba5da136c168fe0de7f7637124ce1a6dcd11
Juan Ignacio Molina <juanignaciomln@gmail.com>
2019-10-17 02:20:46 -0300

    Added basic message processing

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	ID    float64 `json:"id"`
+	Value string  `json:"value"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+	Ok      bool   `json:"ok"`
+}
+
+func Handler(request Request) (Response, error) {
+	return Response{
+		Message: fmt.Sprintf("Process Request ID %f", request.ID),
+		Ok:      true,
+	}, nil
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	lambda.Start(Handler)
 }
